d52033a380dd2675a49c2fa639e319524491fbaf
Daniel Corin <dcorin6@gmail.com>
2023-12-09 19:24:23 -0500

    This is a disaster but it's something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,139 @@
-import Image from 'next/image'
+"use client"
+
+import { Button, ChatList, IChatItemProps, Input } from "react-chat-elements";
+import Markdown from "react-markdown"
+import { MonacoDiffEditor } from 'react-monaco-editor'
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
+import 'react-chat-elements/dist/main.css'
+import { createRef, useState } from "react";
+
+
+const originalCode = `# Vision for a Text Sculpting Tool
+
+## Introduction
+I'm captivated by the idea of constructing documents in a non-traditional, more dynamic manner.
+My aim is to conceptualize a tool that revolutionizes the way we create text, moving away from the conventional process of writing and refining sentences one by one.
+
+## Concept Overview
+Instead of the classic approach to writing, I envision a method akin to sculpting.
+The tool I imagine would provide the capability to manipulate and form text in a way that is more organic and intuitive.
+`;
+const modifiedCode = `# Vision for a Text Sculpting Tool
+
+## Introduction
+I am interested in building a tool that allows me to construct a document in a non-linear, dynamic fashion.
+My motivation stems from the struggle of traditional writing where I often find myself restructuring and refining as I go, which disrupts the flow of ideas.
+
+## Concept Overview
+I have always found that I communicate best in conversation, where ideas flow naturally and can be refined afterward.
+This revelation leads me to envision a tool that empowers me to build a document as if I were sculpting text.
+Through it, I can organically develop and refine my ideas, much as a sculptor works with clay, without being constrained by the linearity of traditional writing.
+`;
+
+
 
 export default function Home() {
+  const inputReference = createRef<HTMLInputElement>();
+  const [messages, setMessages] = useState<IChatItemProps[]>([
+    {
+      id: 1, // Added id property to satisfy IChatItemProps
+      avatar: 'https://via.placeholder.com/40',
+      alt: 'Reactjs',
+      title: 'Chat',
+      subtitle: '',
+      date: new Date(),
+      unread: 0,
+    },
+  ]);
+
+  const submitMessage = () => {
+    console.log("submit")
+    const inputValue = inputReference.current?.value || '';
+    if (inputReference.current) {
+      inputReference.current.value = '';
+    }
+    console.log(inputValue)
+    if (inputValue.trim()) {
+      const newMessage = {
+        id: messages.length + 1,
+        avatar: 'https://via.placeholder.com/40',
+        alt: 'Reactjs',
+        title: 'User',
+        subtitle: inputValue,
+        date: new Date(),
+        unread: 0,
+        style: {
+          '--rce-citem-body-bottom-title-white-space': 'normal',
+          '--rce-citem-body-bottom-title-overflow': 'visible',
+          '--rce-citem-body-bottom-title-text-overflow': 'clip',
+        }
+      };
+      setMessages([...messages, newMessage]);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main style={{ height: '100vh' }}>
+      <PanelGroup direction="horizontal">
+        <Panel defaultSizePercentage={40} minSizePercentage={15}>
+          <MonacoDiffEditor
+            width="100%"
+            height="100%"
+            language="markdown"
+            original={originalCode}
+            value={modifiedCode}
+            options={{
+              minimap: { enabled: false },
+              scrollbar: {
+                verticalScrollbarSize: 5,
+                horizontalScrollbarSize: 5
+              }
+            }}
+          />
+        </Panel>
+        <PanelResizeHandle className="mx-1 w-1 bg-slate-300" />
+        <Panel defaultSizePercentage={40} minSizePercentage={15}>
+          <div style={{ overflow: 'auto' }}>
+            <Markdown className={`markdown`}>
+              {modifiedCode}
+            </Markdown>
+          </div>
+        </Panel>
+        <PanelResizeHandle className="mx-1 w-1 bg-slate-300" />
+        <Panel defaultSizePercentage={20} minSizePercentage={15}>
+          <div style={{ overflow: 'auto' }}>
+            <ChatList
+              className='chat-list'
+              id="chat-list-1"
+              lazyLoadingImage="https://via.placeholder.com/40"
+              dataSource={messages}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <Input
+              referance={inputReference}
+              placeholder='What do you think?'
+              multiline={true}
+              maxHeight={200}
+              minHeight={40}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  event.preventDefault();
+                  submitMessage()
+                }
+              }}
+              rightButtons={
+                <Button
+                  color='white'
+                  backgroundColor='black'
+                  text='Send'
+                  onClick={() => {
+                    submitMessage()
+                  }}
+                />
+              }
+            />
+          </div>
+        </Panel>
+      </PanelGroup>
     </main>
   )
 }
