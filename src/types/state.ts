8522a828a2b18009b3e1eed9933698b919ff91ba
Dan Corin <dcorin6@gmail.com>
2023-12-27 21:16:04 -0500

    Add regenerate button (#5)

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,11 +1,16 @@
 import { Message } from "./message";
 
+export type EditorContentTarget = {
+  content: string;
+  timestamp: number;
+}
+
 export type ApplicationState = {
   messages: Message[];
   document: string;
   proposedDocument: string;
   loadingResults: boolean;
   // we can use this hook to set the content of the rich text editor on "reject"
-  rejectedDocumentHook: string;
+  rejectedDocumentHook: EditorContentTarget;
   proposingChanges: boolean;
 };
