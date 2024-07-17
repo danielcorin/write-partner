bbae213d4c4c1c82785b8d80798818c92dffcbc6
Dan Corin <dcorin6@gmail.com>
2024-07-16 21:01:18 -0400

    Add flake, update deps

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+
+          shellHook = ''
+            echo "Node.js development environment activated"
+            echo "Node version: $(node --version)"
+            echo "NPM version: $(npm --version)"
+          '';
+        };
+      }
+    );
+}
+
